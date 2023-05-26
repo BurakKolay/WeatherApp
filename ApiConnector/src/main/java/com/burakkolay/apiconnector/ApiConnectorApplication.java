ce42e07db85ae25b2c947370d8715b14c0c46eba
BurakKolay <burakkolay487@gmail.com>
2023-05-26 22:35:23 +0300

    Created repo and api connector service

---
+++ ApiConnector/src/main/java/com/burakkolay/apiconnector/ApiConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.burakkolay.apiconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiConnectorApplication.class, args);
+	}
+
+}
---
+++ ApiConnector/src/test/java/com/burakkolay/apiconnector/ApiConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burakkolay.apiconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
