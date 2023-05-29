3000c989876ad781ab10be9261e966e92f9dad0d
BurakKolay <burakkolay487@gmail.com>
2023-05-29 17:58:33 +0300

    added openFeign controller and enabled

--- ApiConnector/src/main/java/com/burakkolay/apiconnector/ApiConnectorApplication.java
+++ ApiConnector/src/main/java/com/burakkolay/apiconnector/ApiConnectorApplication.java
@@ -2,12 +2,13 @@ package com.burakkolay.apiconnector;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ApiConnectorApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiConnectorApplication.class, args);
 	}
-
 }
