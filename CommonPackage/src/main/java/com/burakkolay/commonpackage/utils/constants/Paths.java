1f1221c5961e6ce5081d32ea4ee3293baf9ceb89
BurakKolay <burakkolay487@gmail.com>
2023-05-30 17:29:27 +0300

    config paths

---
+++ CommonPackage/src/main/java/com/burakkolay/commonpackage/utils/constants/Paths.java
@@ -0,0 +1,12 @@
+package com.burakkolay.commonpackage.utils.constants;
+
+public class Paths {
+    public static final String ConfigurationBasePackage = "com.burakkolay.commonpackage.configuration";
+
+    public static class User {
+        public static final String ServiceBasePackage = "com.burakkolay.userservice";
+    }
+    public static class Weather {
+        public static final String ServiceBasePackage = "com.burakkolay.apiconnector";
+    }
+}
