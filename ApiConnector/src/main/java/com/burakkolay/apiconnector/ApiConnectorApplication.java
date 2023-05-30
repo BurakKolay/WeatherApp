280b1179c6b8cdd93b16bf0054bd2890e6225b38
BurakKolay <burakkolay487@gmail.com>
2023-05-30 17:29:07 +0300

    added scan packages for config

--- ApiConnector/src/main/java/com/burakkolay/apiconnector/ApiConnectorApplication.java
+++ ApiConnector/src/main/java/com/burakkolay/apiconnector/ApiConnectorApplication.java
@@ -1,10 +1,11 @@
 package com.burakkolay.apiconnector;
 
+import com.burakkolay.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.Weather.ServiceBasePackage})
 @EnableFeignClients
 public class ApiConnectorApplication {
 
--- UserService/src/main/java/com/burakkolay/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/burakkolay/userservice/UserServiceApplication.java
@@ -1,9 +1,10 @@
 package com.burakkolay.userservice;
 
+import com.burakkolay.commonpackage.utils.constants.Paths;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {Paths.ConfigurationBasePackage, Paths.User.ServiceBasePackage})
 public class UserServiceApplication {
 
     public static void main(String[] args) {
