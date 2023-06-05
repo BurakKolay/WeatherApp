bf3289144b9e5f4b811674ab4527ab5d88dc224d
BurakKolay <burakkolay487@gmail.com>
2023-06-05 23:13:58 +0300

    Changed WeatherDTO

--- WeatherService/src/main/java/com/burakkolay/weatherservice/api/controller/WeatherController.java
+++ WeatherService/src/main/java/com/burakkolay/weatherservice/api/controller/WeatherController.java
@@ -1,9 +1,8 @@
 package com.burakkolay.weatherservice.api.controller;
 
-import com.burakkolay.commonpackage.business.dto.response.GetWeatherDTO;
 import com.burakkolay.weatherservice.business.concretes.WeatherManager;
+import com.burakkolay.weatherservice.entities.Weather;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,9 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class WeatherController {
     private final WeatherManager weatherManager;
 
-
     @GetMapping
-    public GetWeatherDTO getWeather(@RequestParam String city){
+    public Weather getWeather(@RequestParam String city){
         return weatherManager.getWeather(city);
     }
 }
