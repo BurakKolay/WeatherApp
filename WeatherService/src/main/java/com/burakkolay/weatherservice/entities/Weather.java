2da78320937a4d436cf67bddf08368e446339d00
BurakKolay <burakkolay487@gmail.com>
2023-06-05 23:15:14 +0300

    Created weather entity

---
+++ WeatherService/src/main/java/com/burakkolay/weatherservice/entities/Weather.java
@@ -0,0 +1,32 @@
+package com.burakkolay.weatherservice.entities;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.LinkedList;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Weather {
+    private String cod;
+    private LinkedList<List> list;
+
+    @Getter
+    @Setter
+    private static class List{
+        private Main main;
+        private String dt_txt;
+    }
+    @Getter
+    @Setter
+    private static class Main{
+        private double temp;
+        private double temp_min;
+        private double temp_max;
+    }
+}
