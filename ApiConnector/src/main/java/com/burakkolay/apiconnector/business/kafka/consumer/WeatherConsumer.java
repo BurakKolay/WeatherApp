c94e2e8c97d6695626ad134fa1a181871b9043ac
BurakKolay <burakkolay487@gmail.com>
2023-06-05 23:14:48 +0300

    created consumer for logging

--- ApiConnector/src/main/java/com/burakkolay/apiconnector/business/kafka/consumer/WeatherConsumer.java
+++ ApiConnector/src/main/java/com/burakkolay/apiconnector/business/kafka/consumer/WeatherConsumer.java
@@ -1,22 +1,36 @@
 package com.burakkolay.apiconnector.business.kafka.consumer;
-
-
+import com.burakkolay.apiconnector.business.abstracts.LogService;
+import com.burakkolay.apiconnector.entities.Log;
+import com.burakkolay.apiconnector.repository.LogRepository;
+import com.burakkolay.commonpackage.business.dto.response.LogDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Slf4j
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 public class WeatherConsumer {
-//    private final WeatherManager manager;
-//    @KafkaListener(
-//            topics = "get-weather",
-//            groupId = "getting-weather"
-//    )
-//    public void consume(String city){
-//        GetWeatherDTO weather = manager.getWeather(city);
-//        log.info(weather.toString());
-//        manager.sendWeather(weather);
-//    }
+    private final LogService service;
+
+    @PostConstruct
+    public void createDb(){
+        service.add(new Log());
+    }
+    @KafkaListener(
+            topics = "logging",
+            groupId = "logging-for-db"
+    )
+    public void consume(LogDTO log) {
+        System.out.println(log.getUsername());
+        Log logs = new Log();
+        logs.setCreateDate(log.getCreateDate());
+        logs.setUsername(log.getUsername());
+        logs.setOperations(log.getOperations());
+        logs.setId(UUID.randomUUID().toString());
+        service.add(logs);
+    }
 }
