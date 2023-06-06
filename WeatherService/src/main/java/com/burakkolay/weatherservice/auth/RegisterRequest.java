8d9c6ccda077d4dc9b6c6c4496f59aaae5a10431
BurakKolay <burakkolay487@gmail.com>
2023-06-06 14:09:49 +0300

    added validation for User username,pass,name

--- WeatherService/src/main/java/com/burakkolay/weatherservice/auth/RegisterRequest.java
+++ WeatherService/src/main/java/com/burakkolay/weatherservice/auth/RegisterRequest.java
@@ -1,5 +1,7 @@
 package com.burakkolay.weatherservice.auth;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,9 +12,13 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterRequest {
-
+    @Column(length = 100,nullable = false)
     private String firstName;
+    @Column(length = 100,nullable = false)
     private String lastName;
+    @Column(length = 100,unique = true,nullable = false)
     private String username;
+    @Column(length = 100,nullable = false)
+    @Size(min = 5,message = "Your password must be a minimum of 5 characters")
     private String password;
 }
--- WeatherService/src/main/java/com/burakkolay/weatherservice/entities/User.java
+++ WeatherService/src/main/java/com/burakkolay/weatherservice/entities/User.java
@@ -1,6 +1,8 @@
 package com.burakkolay.weatherservice.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -22,9 +24,14 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100,nullable = false)
     private String firstName;
+    @Column(length = 100,nullable = false)
     private String lastName;
+    @Column(length = 100,unique = true,nullable = false)
     private String username;
+    @Column(length = 100,nullable = false)
+    @Size(min = 5,message = "Your password must be a minimum of 5 characters")
     private String password;
     private ArrayList<String> cities;
     @Enumerated(EnumType.STRING)
