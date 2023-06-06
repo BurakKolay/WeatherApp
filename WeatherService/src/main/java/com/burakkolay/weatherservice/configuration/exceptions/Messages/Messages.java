45897b26242d663a066d88365f135b63abc9e572
BurakKolay <burakkolay487@gmail.com>
2023-06-06 14:09:33 +0300

    added messages for city and user

--- WeatherService/src/main/java/com/burakkolay/weatherservice/configuration/exceptions/Messages/Messages.java
+++ WeatherService/src/main/java/com/burakkolay/weatherservice/configuration/exceptions/Messages/Messages.java
@@ -3,5 +3,10 @@ package com.burakkolay.weatherservice.configuration.exceptions.Messages;
 public class Messages {
     public static class User{
         public static final String CityAlreadyExists= "CITY_ALREADY_EXITS";
+        public static String UserCityNotExists= "USER_DOESNT_HAVE_CITY";
+        public static final String UserAlreadyExists= "USER_ALREADY_EXITS";
+    }
+    public static class Weather{
+        public static final String CityNotFound = "CITY_NOT_FOUND";
     }
 }
